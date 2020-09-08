573257a4f5b44c2cad89a3b1f564c83774a1bc45
Just be free <justbefree@126.com>
2020-09-08 17:12:44 +0800

    Add back icon

--- examples/App.vue
+++ examples/App.vue
@@ -1,5 +1,10 @@
 <template>
   <div id="app" :style='{ minHeight: `${height}px`, width: `${width}px` }'>
+    <div class="header">
+      <div class="back-icon" title="go back" @click="back">
+        <yn-iconfont name="iconback-black"></yn-iconfont>
+      </div>
+    </div>
     <router-view />
   </div>
 </template>
@@ -12,11 +17,16 @@ export default {
       width: document.documentElement.clientWidth,
       height: document.documentElement.clientHeight
     }
+  },
+  methods: {
+    back() {
+      this.$router.back();
+    }
   }
 };
 </script>
 
-<style>
+<style scoped="scoped">
 *{
   margin: 0;
   padding: 0;
@@ -25,10 +35,15 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  /* text-align: center; */
   color: #2c3e50;
   position: relative;
   height: 100%;
   width: 100%;
+}
+.back-icon {
+  position: absolute;
+  left: 20px;
+  top: 20px;
+  cursor: pointer;
 }
 </style>
