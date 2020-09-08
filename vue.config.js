46175a6cb04bb9d84cd3968363418f248f507f43
Just be free <justbefree@126.com>
2020-09-08 15:47:52 +0800

    update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,11 +2,17 @@
 * @Author: Just be free
 * @Date:   2020-09-04 11:11:59
 * @Last Modified by:   Just be free
-* @Last Modified time: 2020-09-08 09:54:21
+* @Last Modified time: 2020-09-08 15:45:20
 * @E-mail: justbefree@126.com
 */
 process.env.VUE_APP_VERSION = require('./package.json').version;
+const publicPath = process.env.NODE_ENV === 'production' ? "/vue-amp2.x/" : "/";
 module.exports = {
+  publicPath,
+  outputDir: "docs",
+  css: {
+    extract: false
+  },
   devServer: {
     proxy: {
       "/Hotel": {
